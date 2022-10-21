3cd2bc6e4fd28ad945d57dbe51c9addb505594a9
Jcabezas.burgos <jcabezas.burgos@gmail.com>
2022-10-21 21:14:13 +0000

    sqlmodeler

--- src/models.py
+++ src/models.py
@@ -8,26 +8,53 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = "User"
+   
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    UserName = Column(String(100))
+    FirstName = Column(String(50))
+    LastName = Column(String(50))
+    Email = Column(String(50))
+    Favorites = relationship ("Favorite")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Favorite(Base):
+    __tablename__ = "Favorite"
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    Name = Column(String(250))
+    Userid = Column(Integer, ForeignKey("User.id"))
+     
+    
 
-    def to_dict(self):
-        return {}
+class Vehicle(Base):
+    __tablename__ = "Vehicle"
+   
+    id = Column(Integer,primary_key=True )
+    Name = Column(String(250))
+    Model = Column(String(250))
+    Crew = Column(Integer)
+    vehicle_class = Column(String(100))
+    Pilots = Column(String(100))
+
+class People(Base):
+    __tablename__ = "People"
+   
+    id = Column(Integer, primary_key=True)
+    Name = Column(String(250))
+    Heigth = Column(Integer) 
+    HairColor = Column(String(250))
+    SkinColor = Column(String(250))
+    eye_color = Column(String(250))
+    Birth_Year = Column(String(250))
+
+class Planets(Base):
+    __tablename__ = "Planets"
+   
+    id = Column(Integer, primary_key=True)
+    Name = Column(String(250))
+    Climate = Column(String(250))
+    Gravity = Column(String(250)) 
+    Residents = Column(String(250))
 
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+render_er(Base, "diagram.png")
